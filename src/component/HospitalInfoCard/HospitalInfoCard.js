69a4763e23aad9d3a4567ad33553ae17aa53d8c0
mudit999 <500062539@stu.upes.ac.in>
2021-05-27 13:01:43 +0530

    State and City added in details of hospital

--- src/component/HospitalInfoCard/HospitalInfoCard.js
+++ src/component/HospitalInfoCard/HospitalInfoCard.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import { Typography } from '@material-ui/core';
 
 const HospitalInfoCard = ( {data} ) => {
+
     return (
         <div>
             <Card elevation = {4}>
@@ -13,10 +14,12 @@ const HospitalInfoCard = ( {data} ) => {
                     subheader = {data.PhoneNo}
                 />
                 <CardContent>
-                    <Typography variant = 'body1' color = 'textSecondary'>
+                    <Typography variant = 'body1' color = 'textSecondary' component = {'span'}>
 
                         <ul className = 'ul-data'>
                             <li>{data.Address}</li>
+                            <li>{data.State}</li>
+                            <li>{data.City}</li>
                             <li>{data.CovidPatientsIntake}</li>
                             <li>{data.Beds}</li>
                             <li>{data.WardsAvailable}</li>
